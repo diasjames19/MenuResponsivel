362ca1856d0e834aa9f221b2ecaaa6cacad8108a
jamesalvesdias37 <James_Alves_Dias@hotmail.com>
2023-01-14 18:24:13 -0300

    Criacao de pasta

---
+++ js/funcao.js
@@ -0,0 +1,9 @@
+function clickMenu(){
+    let op = document.getElementById('opcao')
+   if(op.style.display == 'block'){
+       op.style.display = 'none'
+   } else{
+       op.style.display = 'block'
+   }
+   
+}
